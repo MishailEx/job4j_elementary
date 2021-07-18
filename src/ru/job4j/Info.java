8505e2708405971f6c5af6c88447ff916745005d
MishailEx <Mishail.avram@mail.ru>
2021-07-18 11:16:07 +0300

    3. Арифметические операции. [#185]

--- src/ru/job4j/Info.java
+++ src/ru/job4j/Info.java
@@ -2,7 +2,18 @@ package ru.job4j;
 
 public class Info {
     public static void main(String[] args) {
-        System.out.println("18.07.2021");
+
+
+        int age = 18;
+        boolean sex = true;
+        float height = 180.5f;
+        int brick = 28400;
+        int bacterium = (int) (5*Math.pow(10,30));
+        double spaceDistance = 7*Math.pow(10,40);
+        boolean resolution = false;
+        int file = 524;
+
+        System.out.println(bacterium);
     }
 }
 
