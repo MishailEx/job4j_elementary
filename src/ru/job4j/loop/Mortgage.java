08d6cabfdb47bedfca251a85f46b4f2e1b62c83e
MishailEx <Mishail.avram@mail.ru>
2021-07-20 16:42:09 +0300

    5.7. Ипотека [#156315 #179750]

--- src/ru/job4j/loop/Mortgage.java
+++ src/ru/job4j/loop/Mortgage.java
@@ -3,9 +3,8 @@ package ru.job4j.loop;
 public class Mortgage {
     public static int year(double amount, int salary, double percent) {
         int year = 0;
-        double am = amount;
-        while (am > 0) {
-            am = am + (am * (percent / 100)) - salary;
+        while (amount > 0) {
+            amount = amount + (amount * (percent / 100)) - salary;
             year++;
         }
         return year;
