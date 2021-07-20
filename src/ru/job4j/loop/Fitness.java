b36b21ea4e6a209df5cf6997badf8c8be162a997
MishailEx <Mishail.avram@mail.ru>
2021-07-20 17:30:00 +0300

    5.4. Протеиновая диета [#156313 #179700]

--- src/ru/job4j/loop/Fitness.java
+++ src/ru/job4j/loop/Fitness.java
@@ -3,11 +3,9 @@ package ru.job4j.loop;
 public class Fitness {
     public static int calc(int ivan, int nik) {
         int month = 0;
-        int ivanProgress = ivan;
-        int nikProgress = nik;
-        while (nikProgress >= ivanProgress) {
-            nikProgress *= 2;
-            ivanProgress *= 3;
+        while (nik >= ivan) {
+            nik *= 2;
+            ivan *= 3;
             month++;
         }
         return month;
