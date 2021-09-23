7b7dac212a34c24395a98e0f12e64fa7b8a695dd
MishailEx <Mishail.avram@mail.ru>
2021-09-23 15:10:09 +0300

    3. Перегрузить метод max для трех чисел [#122644 #180868]

--- src/ru/job4j/array/Matrix.java
+++ src/ru/job4j/array/Matrix.java
@@ -1,5 +1,11 @@
 package ru.job4j.array;
 
+import javax.sound.sampled.AudioFormat;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 
 public class Matrix {
@@ -15,11 +21,17 @@ public class Matrix {
 
     public static void main(String[] args) {
         int[][] arr = multiple(9);
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr[i].length; j++) {
-                System.out.print(arr[i][j] + " ");
+        try (FileOutputStream out = new FileOutputStream("result.txt")) {
+            for (int i = 0; i < arr.length; i++) {
+                for (int j = 0; j < arr[i].length; j++) {
+                    System.out.print(arr[i][j] + " ");
+                    out.write((arr[i][j] + " ").getBytes(StandardCharsets.UTF_8));
+                }
+                System.out.println();
+                out.write(System.lineSeparator().getBytes(StandardCharsets.UTF_8));
             }
-            System.out.println();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
